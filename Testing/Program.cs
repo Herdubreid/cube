bf5f2564aeb7d01d8cb89cd93d82a89d3a1b4b20
Finnur Bragason <fbragason@outlook.com>
2021-05-11 13:44:10 +0800

    data tab added

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -2,13 +2,30 @@
 using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.Linq;
+using System.Threading.Tasks;
 
-namespace Testing
+namespace Celin
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static async Task Main(string[] args)
         {
+            var e1 = new AIS.Server("https://demo.steltix.com/jderest/v2/");
+            e1.AuthRequest.username = "demo";
+            e1.AuthRequest.password = "demo";
+
+            var rs = await e1.RequestAsync(new AIS.DiscoverUBERequest
+            {
+                reportName = "R45300",
+                reportVersion = "ZJDE0001"
+            });
+
+            foreach (var d in rs.dataSelectionColumns)
+            {
+                Console.WriteLine("{0} {1}", d.dictItem, d.displayString);
+            }
+            Console.ReadKey();
         }
     }
 }
